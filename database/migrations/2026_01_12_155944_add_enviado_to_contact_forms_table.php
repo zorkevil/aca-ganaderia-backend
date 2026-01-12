d64bec8e84f7f0eb321d76b390ae04bf426e353c
Juan Ignacio Spadoni <yo_juan_kapo@hotmail.com>
2026-01-12 13:00:47 -0300

    Agregado de campo enviado

---
+++ database/migrations/2026_01_12_155944_add_enviado_to_contact_forms_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contact_forms', function (Blueprint $table) {
+            $table->boolean('enviado')
+                ->default(false)
+                ->after('section');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contact_forms', function (Blueprint $table) {
+            $table->dropColumn('enviado');
+        });
+    }
+};
